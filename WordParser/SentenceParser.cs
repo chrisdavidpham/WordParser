51739b51e0f7a31cfdcefdf1864b67d5daf0fc76
Chris Pham <50644282+chrisdavidpham@users.noreply.github.com>
2023-11-28 22:51:11 -0600

    Updated documentation

--- WordParser/SentenceParser.cs
+++ WordParser/SentenceParser.cs
@@ -6,7 +6,7 @@ using System.Text.RegularExpressions;
 namespace WordParser
 {
     /// <summary>
-    /// Parses words in a sentence and itterates each word.
+    /// Parses the words in a sentence into a list, and can parse words into DistinctCountBetweenWords.
     /// </summary>
     public class SentenceParser
     {
@@ -20,17 +20,17 @@ namespace WordParser
         /// Parses a sentence's words into DistinctCountBetween words
         /// </summary>
         /// <param name="sentence"></param>
-        /// <returns></returns>
+        /// <returns>String value of the sentence words parsed as DistinctCountBetweenWords</returns>
         public string ParseWords(string sentence)
         {
             return ParseWords(ParseSentence(sentence));
         }
 
         /// <summary>
-        /// Parses words ine DistinctCountBetween words
+        /// Parses words into DistinctCountBetween words
         /// </summary>
         /// <param name="words"></param>
-        /// <returns></returns>
+        /// <returns>String value of the words parsed as DistinctCountBetweenWords</returns>
         public string ParseWords(List<string> words)
         {
             var stringBuilder = new StringBuilder();
@@ -44,10 +44,10 @@ namespace WordParser
         }
 
         /// <summary>
-        /// Splits a sentence by words. A word is a string of consecutive alphanumeric characters,
-        /// or a string of consecutive special characters.
+        /// Parses a sentence's words. A word is a string of consecutive alphanumeric characters or a string of consecutive special characters.
         /// </summary>
         /// <param name="sentence"></param>
+        /// <returns>List of words in the sentence</returns>
         public List<string> ParseSentence(string sentence)
         {
             var wordList = Regex.Split(sentence, "([^0-9a-zA-Z]+|[0-9a-zA-Z]+)").ToList();
